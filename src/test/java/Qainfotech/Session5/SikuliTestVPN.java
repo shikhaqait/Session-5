27bbb09ca049585df5a8f9b73cf98f469a704998
Shikha Mudgal <shikhamudgal@QAIT.COM>
2017-02-15 12:40:44 +0530

    Updating the code

--- src/test/java/Qainfotech/Session5/SikuliTestVPN.java
+++ src/test/java/Qainfotech/Session5/SikuliTestVPN.java
@@ -1,5 +1,26 @@
 package Qainfotech.Session5;
 
+import org.sikuli.script.*;
+import org.testng.annotations.Test;
+
 public class SikuliTestVPN {
+	
+	@Test
+	public void sikulitest() throws InterruptedException{
+		 Screen scr = new Screen();
+         try{
+        	    scr.type("d",Key.WIN);
+        	 	scr.doubleClick("C:\\Users\\shikhamudgal\\workspace4\\Session5\\Img\\vpnShortcut (1).png", 0);
+                scr.click("C:\\Users\\shikhamudgal\\workspace4\\Session5\\Img\\vpnShortcut (2).png", 0);
+                scr.wait("C:\\Users\\shikhamudgal\\workspace4\\Session5\\Img\\vpnShortcut (3).png",20);
+                scr.click("C:\\Users\\shikhamudgal\\workspace4\\Session5\\Img\\vpnShortcut (4).png");
+                scr.paste("Compassqa9");
+                scr.click("C:\\Users\\shikhamudgal\\workspace4\\Session5\\Img\\vpnShortcut (5).png");
+
+         }
+         catch(FindFailed e){
+                 e.printStackTrace();
+         }
+	}
 
 }
